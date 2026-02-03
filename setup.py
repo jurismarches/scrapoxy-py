a6a34ccee023937effde87d4fc3ce8fd949e1ac5
Maxime Morinière <m.moriniere@octopusmind.info>
2026-02-03 15:17:38 +0100

    Packaging fixes

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ except ImportError:
 
 
 setup(
-    name='scrapoxy',
+    name="scrapoxy",
     packages=find_packages(),
-    install_requires=['requests'],
-    version='1.11',
-    description='Use Scrapoxy with Scrapy',
-    author='Fabien Vauchelles',
-    author_email='fabien@vauchelles.com',
-    url='https://github.com/fabienvauchelles/scrapoxy-python-api',
-    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.11',
-    keywords=['crawler', 'crawling', 'scrapoxy', 'scrapy', 'scraper', 'scraping'],
+    install_requires=["requests"],
+    version="1.12",
+    description="Use Scrapoxy with Scrapy",
+    author="OctopusMind Team",
+    author_email="it@octopusmind.info",
+    url="https://github.com/jurismarches/scrapoxy-py",
+    download_url="https://github.com/jurismarches/scrapoxy-py/archive/refs/heads/main.zip",
+    keywords=["crawler", "crawling", "scrapoxy", "scrapy", "scraper", "scraping"],
     classifiers=[],
 )
